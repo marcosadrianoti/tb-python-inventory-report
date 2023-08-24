bc184fd8ba9697e993c2cab97d1aca709973ef5a
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-08-24 17:05:28 -0300

    Req 04 done

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
 from typing import Dict, Type, List
+import json
 
 
 class Importer(ABC):
@@ -12,8 +13,26 @@ class Importer(ABC):
         raise NotImplementedError
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+        self.list_products: List[Product] = []
+
+    def import_data(self) -> List[Product]:
+        with open(self.path, 'r') as file:
+            content = file.read()
+            products_json = json.loads(content)
+        for product in products_json:
+            object_product = Product(
+                id=product["id"],
+                product_name=product["product_name"],
+                company_name=product["company_name"],
+                manufacturing_date=product["manufacturing_date"],
+                expiration_date=product["expiration_date"],
+                serial_number=product["serial_number"],
+                storage_instructions=product["storage_instructions"])
+            self.list_products.append(object_product)
+        return self.list_products
 
 
 class CsvImporter:
