a7a7405a500d66797bd31176e528eca6a2cac9f1
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-08-24 16:03:36 -0300

    Req 03 done

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,8 +1,15 @@
-from typing import Dict, Type
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
+from typing import Dict, Type, List
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        raise NotImplementedError
 
 
 class JsonImporter:
