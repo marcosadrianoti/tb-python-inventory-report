ceb61977e8d7d0c72f787a5eb94297ed65bda1cb
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-08-25 15:17:22 -0300

    Req 07 temp

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,68 @@
-class SimpleReport:
-    pass
+from datetime import datetime
+from typing import Dict, List
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self) -> None:
+        self.inventories: List[Inventory] = []
+        self.oldest_manuf_date = datetime.now()
+        self.closest_exp_date = datetime.strptime("2500-09-30", "%Y-%m-%d")
+        self.largest_inventory_company = ''
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def generate(self) -> str:
+        self.get_oldest()
+        oldest = self.oldest_manuf_date
+        self.get_closest()
+        closet = self.closest_exp_date
+        self.get_largest()
+        company = self.largest_inventory_company
+
+        result = (
+            f"Oldest manufacturing date: {oldest.strftime('%Y-%m-%d')}\n"
+            f"Closest expiration date: {closet.strftime('%Y-%m-%d')}\n"
+            f"Company with the largest inventory: {company}"
+        )
+
+        return result
+
+    def get_largest(self) -> None:
+        company: Dict[str, int] = dict()
+        # company: Dict = dict()
+
+        for inventory in self.inventories:
+            for product in inventory.data:
+                company_name = product.company_name
+                if company_name in company:
+                    company[company_name] += 1
+                else:
+                    company[company_name] = 1
+
+        self.largest_inventory_company = max(
+            company, key=company.get
+        )
+
+    def get_oldest(self) -> None:
+        for inventory in self.inventories:
+            for product in inventory.data:
+                manufacturing_date = datetime.strptime(
+                    product.manufacturing_date, "%Y-%m-%d"
+                )
+                if manufacturing_date < self.oldest_manuf_date:
+                    self.oldest_manuf_date = manufacturing_date
+
+    def get_closest(self) -> None:
+        for inventory in self.inventories:
+            for product in inventory.data:
+                expiration_date = datetime.strptime(
+                    product.expiration_date, "%Y-%m-%d"
+                )
+                if (
+                    expiration_date >= datetime.now()
+                    and expiration_date < self.closest_exp_date
+                ):
+                    self.closest_exp_date = expiration_date
